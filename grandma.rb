dd997a050bf29b1aad336ba3d792f1af82b3c110
cnlucke <cristy.lucke@gmail.com>
2017-12-16 18:14:23 -0500

    Done.

--- grandma.rb
+++ grandma.rb
@@ -13,12 +13,10 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase == phrase.upcase
-    if phrase == "I LOVE YOU GRANDMA!"
-      "I LOVE YOU TOO PUMPKIN!"
-    else
-      "NO, NOT SINCE 1938!"
-    end
+  if phrase == phrase.upcase && phrase != "I LOVE YOU GRANDMA!"
+    "NO, NOT SINCE 1938!"
+  elsif phrase == phrase.upcase && phrase == "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
   else
     "HUH?! SPEAK UP, SONNY!"
   end
